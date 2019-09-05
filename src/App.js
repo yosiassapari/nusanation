a2a20020ef03899e4e37f624f8cfdecc0cd18d9c
yosiassapari <yosiassapari@gmail.com>
2019-09-05 10:20:52 -0700

    add material.js from https://github.com/tleunen/react-mdl

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {Layout, Header, Navigation, Drawer, Content, Textfield, Footer, FooterSection, FooterLinkList} from 'react-mdl';
 import ReactMapGL from "react-map-gl";
 
 function App() {
@@ -11,13 +12,43 @@ function App() {
   })
   return (
     <div>
-      <ReactMapGL {...viewport}
-        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        mapStyle="mapbox://styles/papuanengineer/ck06ijf1p2eu81dpbjqe547el"
-        onViewportChange={(viewport)=>{setViewport(viewport);}}
-      >
-        Select a province
-      </ReactMapGL>
+      <div style={{height: '300px', position: 'relative'}}>
+        <Layout fixedHeader fixedDrawer>
+            <Header title="Title">
+                <Textfield
+                    value=""
+                    onChange={() => {}}
+                    label="Search"
+                    expandable
+                    expandableIcon="search"
+                />
+            </Header>
+            <Drawer title="Title">
+                <Navigation>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                </Navigation>
+            </Drawer>
+            <Content>
+              <ReactMapGL {...viewport}
+                mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+                mapStyle="mapbox://styles/papuanengineer/ck06ijf1p2eu81dpbjqe547el"
+                onViewportChange={(viewport)=>{setViewport(viewport);}}>
+                Select a province
+              </ReactMapGL>
+            </Content>
+        </Layout>
+        </div>
+      <Footer size="mini">
+    <FooterSection type="left" logo="Title">
+        <FooterLinkList>
+            <a href="/">Help</a>
+            <a href="/">Privacy & Terms</a>
+        </FooterLinkList>
+    </FooterSection>
+</Footer>
     </div>
   );
 }
